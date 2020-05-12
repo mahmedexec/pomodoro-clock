33ae291624e32713977e971d2060ab2d9ad80003
Ahmed Tan <ahmedtan@ahmeds-mbp.home>
2020-05-13 00:23:13 +0500

    Updates

---
+++ precache-manifest.e12450bc7594f541830dc9386b9b1a72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99135946f6d5ec72dff5f4f87cd44c1",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d7b3c2badb37c9654110",
+    "url": "/pomodoro-clock/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "556e67e6e1b816c83caa",
+    "url": "/pomodoro-clock/static/css/main.40309efe.chunk.css"
+  },
+  {
+    "revision": "d7b3c2badb37c9654110",
+    "url": "/pomodoro-clock/static/js/2.b33bb5aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.b33bb5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556e67e6e1b816c83caa",
+    "url": "/pomodoro-clock/static/js/main.6dc0874d.chunk.js"
+  },
+  {
+    "revision": "0b124c589b9f86220547",
+    "url": "/pomodoro-clock/static/js/runtime-main.649843f9.js"
+  }
+]);
